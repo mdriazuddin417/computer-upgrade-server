87e6600bf3a6902aef063dc095340b56612fbe58
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-05-26 15:34:35 +0600

    file update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
@@ -22,13 +23,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  console.log(authorization);
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 async function run() {
   try {
     await client.connect();
     const partsCollection = client
       .db("computerUpgrade")
       .collection("all-Parts");
-    const orderCollection = client.db("computerUpgrade").collection("Order");
+    const orderCollection = client.db("computerUpgrade").collection("order");
     const reviewCollection = client
       .db("computerUpgrade")
       .collection("all-reviews");
@@ -36,6 +53,21 @@ async function run() {
     const paymentCollection = client
       .db("computerUpgrade")
       .collection("payments");
+
+    //verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req?.decoded?.email;
+      console.log(requester);
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    };
 
     ////Payment Methods
     app.post("/create-payment-intent", async (req, res) => {
@@ -52,20 +84,74 @@ async function run() {
       });
     });
 
+    //User Collection All set
+    //token
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+
+      res.send({ result, token: token });
+    });
     app.get("/users", async (req, res) => {
       const query = req.query;
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.post("/add-user", async (req, res) => {
-      const user = req.body;
-      const result = await userCollection.insertOne({ ...user });
-      res.send(result);
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
     });
 
+    //setAdmin
+    app.patch(
+      "/user/admin/:email",
+
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        console.log(email);
+        const role = req.body;
+        console.log(role);
+        const filter = { email: email };
+        const updatedDoc = {
+          $set: { ...role },
+        };
+        console.log(updatedDoc);
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        console.log(result);
+        res.send(result);
+      }
+    );
+
+    app.post("/add-user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne({ ...user, role: "user" });
+      res.send(result);
+    });
+    //User end
+
+    //Part Collection Start
     app.get("/all-parts", async (req, res) => {
       const result = await partsCollection.find({}).toArray();
+
       res.send(result);
     });
     app.post("/part", async (req, res) => {
@@ -76,11 +162,35 @@ async function run() {
 
     app.get("/part/:id", async (req, res) => {
       const id = req.params.id;
+
       const query = { _id: ObjectId(id) };
       const result = await partsCollection.findOne(query);
       res.send(result);
     });
+    app.delete("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.deleteOne(query);
+      res.send(result);
+    });
 
+    app.patch("/part/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const part = req.body;
+
+      const filter = { _id: ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          ...part,
+        },
+      };
+      const result = await partsCollection.updateOne(filter, updatedDoc);
+
+      res.send(result);
+    });
+
+    //Reviews Collection all
     app.get("/all-reviews", async (req, res) => {
       const result = await reviewCollection.find({}).toArray();
       res.send(result);
@@ -90,10 +200,24 @@ async function run() {
       const result = await reviewCollection.insertOne({ ...review });
       res.send(result);
     });
+    //Review end
 
-    app.get("/order", async (req, res) => {
-      const query = req.query;
-      const result = await orderCollection.find(query).toArray();
+    //Order Collection Start
+    app.get("/order", verifyToken, async (req, res) => {
+      const email = req.query.email;
+
+      const decodedEmail = req.decoded.email;
+
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const result = await orderCollection.find(query).toArray();
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+    app.get("/admin-order", async (req, res) => {
+      const result = await orderCollection.find({}).toArray();
       res.send(result);
     });
 
@@ -121,17 +245,22 @@ async function run() {
     });
     app.patch("/order/:id", async (req, res) => {
       const id = req.params.id;
-      const payment = req.body;
+      const paymentValue = req.body;
       const filter = { _id: ObjectId(id) };
-
       const updatedDoc = {
         $set: {
-          ...payment,
+          ...paymentValue,
         },
       };
       const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
-      const result = await paymentCollection.insertOne(payment);
       res.send(updatedOrder);
+    });
+    //Order collection end
+
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne({ ...payment });
+      res.send(result);
     });
   } finally {
   }
